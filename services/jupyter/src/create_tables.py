262a3faf4acf94d786a9f93dae25e884e82d4f69
Adrian Martin <adrianmartinparra@gmail.com>
2020-07-06 15:03:41 +0100

    Add docstrings

--- services/jupyter/src/create_tables.py
+++ services/jupyter/src/create_tables.py
@@ -5,12 +5,36 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    '''
+    Drop all Redshift tables present in the drop_table_queries list.
+
+    Args:
+        cur: A psycopg2.cursor object.
+        conn: A psycopg2.connection object.
+
+    Raises:
+        psycopg2.DatabaseError: Exception raised for errors that are related to
+        the database.
+    '''
+
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    '''
+    Create the Redshift tables present in the create_table_queries list.
+
+    Args:
+        cur: A psycopg2.cursor object.
+        conn: A psycopg2.connection object.
+
+    Raises:
+        psycopg2.DatabaseError: Exception raised for errors that are related to
+        the database.
+    '''
+
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- services/jupyter/src/etl.py
+++ services/jupyter/src/etl.py
@@ -5,12 +5,38 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    '''
+    Load data from S3 buckets into Redshift according to the statements present
+    in the copy_table_queries list.
+
+    Args:
+        cur: A psycopg2.cursor object.
+        conn: A psycopg2.connection object.
+
+    Raises:
+        psycopg2.DatabaseError: Exception raised for errors that are related to
+        the database.
+    '''
+
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    '''
+    Insert data from the staging tables into the fact and dimension tables
+    according to the statements present in the insert_table_queries list.
+
+    Args:
+        cur: A psycopg2.cursor object.
+        conn: A psycopg2.connection object.
+
+    Raises:
+        psycopg2.DatabaseError: Exception raised for errors that are related to
+        the database.
+    '''
+
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
@@ -22,7 +48,7 @@ def main():
 
     conn = psycopg2.connect('host={} dbname={} user={} password={} port={}'.format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
